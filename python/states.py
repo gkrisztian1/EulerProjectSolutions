5937f60bb7894efc4ef2eb67504a23d162d64749
GKrisztian <g.krisztian@outlook.com>
2019-05-20 19:27:34 +0200

    init

--- python/states.py
+++
@@ -1,8 +0,0 @@
-group = 100
-
-states = {
-        "solved": [":white_check_mark:", "**Solved**"],
-        "notsolved": [":x:", "Not Solved"],
-        "inprogress": [":large_blue_circle:", "In Progress"],
-        "intact": [":cloud:", "**Intact**"]
-        }
