7bbdcd629834b5e987983adc35b5f12a82b9a111
GKrisztian <g.krisztian@outlook.com>
2019-05-22 10:10:41 +0200

    daw

--- management/sync_readme.py
+++ management/sync_readme.py
@@ -1,21 +1,29 @@
-import os
 
-def load_info(loc):
-    with open(loc, "w") as f:
-        raw = f.read()
+def format_num(num, c):
+    s1 = f'<span style="color:{c}">**{num}**</span>'
+    s2 = f"[{s1}](../problems/ID{num:03})"
+    
+    return s2
 
-root = "../problems"
+w = 25
 
-dirs = os.listdir(root)
-
-df = {
-      'id': [],
-      'title': [],
-      'diff': [],
-      'sol': [],
-      'state':[]
-      }
+done = [1,44, 23, 23]
 
 
-for dr in dirs:
-    print(dr)
+r = 10
+c = 10
+
+assert r * c >= w, f"{r} * {c} = {r * c}  < {w} !"
+
+for i in range(r):
+    if i == 1:
+        print("|-"*c)
+    for j in range(c):
+        num = c * i + j + 1
+        if num <= w:
+            if num in done:
+                print(f"| {format_num(num, 'green')}", end='')
+            else:
+                print(f"| {format_num(num, 'black')}", end='')
+        
+    print()
