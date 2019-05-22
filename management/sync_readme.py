c6191466965c9b5aec4657edf1116b269b0ed220
GKrisztian <g.krisztian@outlook.com>
2019-05-22 09:44:48 +0200

    d

--- management/sync_readme.py
+++ management/sync_readme.py
@@ -1,5 +1,21 @@
 import os
 
+def load_info(loc):
+    with open(loc, "w") as f:
+        raw = f.read()
+
 root = "../problems"
 
 dirs = os.listdir(root)
+
+df = {
+      'id': [],
+      'title': [],
+      'diff': [],
+      'sol': [],
+      'state':[]
+      }
+
+
+for dr in dirs:
+    print(dr)
